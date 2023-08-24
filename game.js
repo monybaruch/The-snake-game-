059bce74c14760b3f2c5f2512b0894c8114eb6df
מוני ברוך <monybaruch@gmail.com>
2023-08-24 13:41:46 +0300

    adding all the Js files and finishing this little project

--- game.js
+++ game.js
@@ -1,18 +1,43 @@
-import { update as updateSnake, draw as drawsSnake, SNAKE_SPEED } from './snake.js';
-
+import { update as updateSnake, draw as drawsSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js';
+import { update as updateFood, draw as drawFood } from './food.js';
+import { outsideGrid } from './grid.js';
 let lastRenderTime = 0;
+
+let gameOver = false;
+
 const gameBoard = document.querySelector('#game-board');
 
 const main = (currentTime) => {
+  if (gameOver) {
+    if (confirm('You Lost! Press ok to restart!')) {
+      window.location = '/';
+    }
+    return;
+  }
+
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
-  console.log('Render');
+
   lastRenderTime = currentTime;
+
   update();
   draw();
 };
 window.requestAnimationFrame(main);
 
-const update = () => updateSnake(console.log('update snake'));
-const draw = () => drawsSnake(gameBoard);
+const update = () => {
+  updateSnake();
+  updateFood();
+  checkDeath();
+};
+
+const draw = () => {
+  gameBoard.innerHTML = '';
+  drawsSnake(gameBoard);
+  drawFood(gameBoard);
+};
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+}
